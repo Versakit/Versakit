51fcf975735ea7cb510101c996e38bac3fa241ac
abining <3094774079@qq.com>
2025-02-19 18:06:11 +0800

    feat: 组件测试报告生成

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,12 @@ export default defineConfig({
     globals: true,
     // 启用类似 jest 的全局测试 API
     coverage: {
-      provider: 'istanbul', // or 'v8'
+      provider: 'istanbul',
       reporter: ['text', 'json', 'html'],
+      // 指定覆盖率统计范围
+      include: ['packages/versakit-ui/**/*.{js,ts,vue}'],
+      // 排除不需要统计的文件
+      exclude: ['packages/**/dist/**', 'packages/**/node_modules/**'],
     },
     environment: 'jsdom',
   },
